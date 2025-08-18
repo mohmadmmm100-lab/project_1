7066c4f74092b0e35694daa38388fc89981505df
mohmadmmm100-lab <mohmadmmm100@gmail.com>
2025-08-18 18:18:33 +0300

    Refactor form validation and insertion logic in form.php

--- project/php/form.php
+++ project/php/form.php
@@ -7,21 +7,30 @@ $errors = [
     'lastname' => '',
     'email' => ''
 ];
-if(isset($_POST['submit'])) {
-$input = " insert into users(firstname,lastname,email) values ('$firstname','$lastname','$email')";
-if (empty($firstname)) {
-    $errors['firstname'] = "الرجاء ادخال الاسم الأول";
-} if (empty($lastname)) {
-    $errors['lastname'] = "الرجاء ادخال الاسم الأخير";
-} if (empty($email)) {
-    $errors['email'] = "الرجاء ادخال البريد الإلكتروني";
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors['email'] = "الرجاء ادخال ايميل صحيح";
-} else {
-if (mysqli_query($conn, $input)) {
-   header("Location: " . $_SERVER['PHP_SELF']);
-} else {
-    echo 'Error: ' . mysqli_error($conn);
-}}
+if (isset($_POST['submit'])) {
+
+    if (empty($firstname)) {
+        $errors['firstname'] = "الرجاء ادخال الاسم الأول";
+    }
+    if (empty($lastname)) {
+        $errors['lastname'] = "الرجاء ادخال الاسم الأخير";
+    }
+    if (empty($email)) {
+        $errors['email'] = "الرجاء ادخال البريد الإلكتروني";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "الرجاء ادخال ايميل صحيح";
+    }
+
+    // only insert if no errors
+    if (!array_filter($errors)) {
+        $input = "INSERT INTO users(firstname, lastname, email) 
+                  VALUES ('$firstname','$lastname','$email')";
+        if (mysqli_query($conn, $input)) {
+            header("Location: " . $_SERVER['PHP_SELF']);
+            exit;
+        } else {
+            echo 'Error: ' . mysqli_error($conn);
+        }
+    }
 }
 ?>
